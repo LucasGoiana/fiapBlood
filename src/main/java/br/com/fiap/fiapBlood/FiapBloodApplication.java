453d8cf52b7bbe0e3d9800280d38e4adee794f10
Lucas Goiana Malicia <lucasgoianam@hotmail.com>
2021-10-03 18:05:39 -0300

    First commit

---
+++ src/main/java/br/com/fiap/fiapBlood/FiapBloodApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fiapBlood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiapBloodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiapBloodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/fiapBlood/FiapBloodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fiapBlood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiapBloodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
