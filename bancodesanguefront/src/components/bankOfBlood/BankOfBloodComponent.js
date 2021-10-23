a60b620cd3842bf5a33c1db2e712828674c18b47
Lucas Goiana Malicia <lucasgoianam@hotmail.com>
2021-10-23 12:07:47 -0300

    front

--- bancodesanguefront/src/components/bankOfBlood/BankOfBloodComponent.js
+++ bancodesanguefront/src/components/bankOfBlood/BankOfBloodComponent.js
@@ -1,16 +1,71 @@
 import React, {Component} from "react";
-import {Button} from 'reactstrap';
+import {Button, Col, Table} from 'reactstrap';
+import {Link} from "react-router-dom";
+import {findAll, remove} from "./BankOfBloodApi";
+
 
 class BankOfBloodComponent extends Component{
     constructor(props) {
         super(props);
+
+        this.state = { bankOfBlood: [] }
+        this.handleDelete = this.handleDelete.bind(this);
+    }
+
+    componentDidMount() {
+        findAll().then( data => this.setState({ bankOfBlood: data }))
+
+    }
+
+    handleDelete(bankOfBlood){
+        remove(bankOfBlood.id).then( () =>{
+            return findAll().then( data => this.setState({ bankOfBlood: data }))
+        })
     }
 
     render() {
+         let { bankOfBlood } = this.state
         return(
-            <div >
-                <Button href="/bankOfBlood/add" color="primary">+ Banco de Sangue</Button>
-            </div>
+            <>
+                <Col xs="12" sm="12" md="12">
+                        <Link to="/bankOfBlood/add" className="alinha btn btn-primary">+ Banco de Sangue</Link>
+                </Col>
+                 <Col xs="12" sm="12" md="12">
+                    <Table>
+                        <thead>
+                            <tr>
+                                <th>
+                                    Nome
+                                </th>
+                                <th>
+                                    Horário de Atendimento
+                                </th>
+                                <th>
+                                    Ações
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        {bankOfBlood.map((bankOfBlood, index) => (
+                            <tr>
+                                <td>
+                                    {bankOfBlood.nome}
+                                </td>
+                                <td>
+                                    {bankOfBlood.horariodeatendimento}
+                                </td>
+                                <td>
+                                    <Button color = "warning" onClick={ () => this.handleDelete(bankOfBlood)}> Edit </Button>
+                                    <Button color = "danger" onClick={ () => this.handleDelete(bankOfBlood)}> Delete </Button>
+                                </td>
+
+                            </tr>
+                            ))}
+
+                        </tbody>
+                    </Table>
+                </Col>
+            </>
         );
     }
 }
